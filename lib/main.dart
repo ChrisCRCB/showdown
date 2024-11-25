a9b58b415d592e788ed9252433a404b837854fd7
Chris Norman <achris@Chriss-Air.broadband>
2024-11-25 15:07:49 +0000

    Only ensure semantics on web.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -16,8 +17,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(final BuildContext context) {
-    WidgetsFlutterBinding.ensureInitialized();
-    RendererBinding.instance.ensureSemantics();
+    if (kIsWeb) {
+      WidgetsFlutterBinding.ensureInitialized();
+      RendererBinding.instance.ensureSemantics();
+    }
     return ProviderScope(
       child: MaterialApp(
         title: 'Showdown',
